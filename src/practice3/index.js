02f641a7de6b2fa93b009cbbb1e7c83f09af51e5
Haoran Lin <tdkgn@outlook.com>
2020-11-14 21:25:21 +0800

    pass test for practice3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,9 @@
-export const getPoetry
+const URL = "https://v1.jinrishici.com/all.json";
+
+// eslint-disable-next-line import/prefer-default-export
+export const getPoetry = () =>
+  fetch(URL)
+    .then((response) => response.json())
+    .then((poetry) =>
+      ["origin", "author", "content"].map((field) => poetry[field])
+    );
