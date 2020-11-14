92b037a73d96e26371984d69deed0101eaf1782d
Haoran Lin <tdkgn@outlook.com>
2020-11-14 21:21:19 +0800

    pass test for practice1

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,6 @@
+const { ADDRESS, PORT, PATH, REGION } = require("./constant");
+const { default: getUrl } = require("./utils");
 
+export const url = getUrl(ADDRESS, PORT, PATH);
+
+export const region = REGION;
